a7552a70a1cb0eb21ccbcd2c387a2ffeeb24cd48
anisingh <anisingh565072@gmail.com>
2026-05-19 21:22:33 +0530

    initial commit

---
+++ Day 1/Coding Questions/Day1.js
@@ -0,0 +1,11 @@
+function ReverseString(str) {
+  let ReversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    ReversedStr = ReversedStr + str[i];
+  }
+  return ReversedStr;
+}
+
+function 
+
+console.log(ReverseString("my name is ankit"));
