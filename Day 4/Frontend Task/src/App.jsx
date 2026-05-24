6c3b0a57831b5bd358183d3dbf34620183bcccc3
anisingh <anisingh565072@gmail.com>
2026-05-24 21:01:12 +0530

    Day 4 Completed

---
+++ Day 4/Frontend Task/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Pagination from "./Components/Pagination";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <Pagination />
+    </div>
+  );
+};
+
+export default App;
